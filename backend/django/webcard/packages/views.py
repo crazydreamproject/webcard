0d1f4d3634c2119afb6e2ee8604d592f3f8ea28f
crazydreamproject <65080564+crazydreamproject@users.noreply.github.com>
2020-08-02 00:07:35 +0900

    Add class to allow permission to edit instance only if owner is myself.

--- backend/django/webcard/packages/views.py
+++ backend/django/webcard/packages/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from django.conf import settings
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, permissions
 from rest_framework.reverse import reverse_lazy
 from .models import Package
 from .serializers import PackageSerializer
@@ -10,10 +10,23 @@ from .serializers import PackageSerializer
 # Create your views here.
 User = get_user_model()
 
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object level permission to only show owners of an object to edit
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD, or OPTIONS request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # check instance owner is myself
+        return obj.author == request.user
+
 class PackageViewSet(viewsets.ModelViewSet):
     queryset = Package.objects.all()
     serializer_class = PackageSerializer
     filter_fields = ('name', 'author', 'stack', 'available', 'category')
+    permission_classes = (IsOwnerOrReadOnly,)
     def get_queryset(self):
         # to list all *MY* packages, OR *ANY* packages that is available (in the market)
         #queryset = Package.objects.filter(author=self.request.user, available=True) # NG
--- backend/django/webcard/stacks/views.py
+++ backend/django/webcard/stacks/views.py
@@ -2,17 +2,30 @@
 #from django.shortcuts import render
 #import django_filters
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, permissions
 from .models import Stack
 from .serializers import StackSerializer, StackListSerializer, AuthorSerializer
 
 # Create your views here.
 User = get_user_model()
 
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object level permission to only show owners of an object to edit
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD, or OPTIONS request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # check instance owner is myself
+        return obj.author == request.user
+
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = AuthorSerializer
     filter_fields = ('username',)
+    permission_classes = (IsOwnerOrReadOnly,)
     def get_queryset(self):
         # just show me, or otherwise anyone can see other's email!
         queryset = User.objects.filter(username=self.request.user)
@@ -21,6 +34,7 @@ class AuthorViewSet(viewsets.ModelViewSet):
 
 class StackViewSet(viewsets.ModelViewSet):
     queryset = Stack.objects.all()
+    permission_classes = (IsOwnerOrReadOnly,)
     #serializer_class = StackSerializer
     def get_serializer_class(self):
         if self.action == 'list':
